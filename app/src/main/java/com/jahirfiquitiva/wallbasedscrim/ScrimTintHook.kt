faf4f50f86e82c61c7d5b9027b150fbcdeab2487
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-07-27 16:09:00 -0500

    :zap: Add copyright. Code cleanup

--- app/src/main/java/com/jahirfiquitiva/wallbasedscrim/ScrimTintHook.kt
+++ app/src/main/java/com/jahirfiquitiva/wallbasedscrim/ScrimTintHook.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2018. Jahir Fiquitiva
+ *
+ * Licensed under the CreativeCommons Attribution-ShareAlike
+ * 4.0 International License. You may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *    http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jahirfiquitiva.wallbasedscrim
 
 import de.robv.android.xposed.IXposedHookLoadPackage
@@ -10,6 +25,10 @@ object ScrimTintHook : IXposedHookLoadPackage {
     private const val METHOD_NAME = "getDarkGradientColor"
     private const val GRADIENT_COLORS_CLASS =
         "com.android.internal.colorextraction.ColorExtractor\$GradientColors"
+    
+    private val classesToModify: Array<String> = arrayOf(
+        "com.android.systemui.globalactions.GlobalActionsDialog\$ActionsDialog",
+        "com.android.systemui.statusbar.phone.ScrimController")
     
     private val replacement: XC_MethodReplacement by lazy {
         object : XC_MethodReplacement() {
@@ -25,24 +44,17 @@ object ScrimTintHook : IXposedHookLoadPackage {
     }
     
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
-        lpparam?.let {
-            if (it.packageName != "com.android.systemui")
+        lpparam?.let { param ->
+            if (param.packageName != "com.android.systemui")
                 return
             
-            try {
-                XposedHelpers.findAndHookMethod(
-                    "com.android.systemui.globalactions.GlobalActionsDialog\$ActionsDialog",
-                    it.classLoader, METHOD_NAME, GRADIENT_COLORS_CLASS, replacement)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-            
-            try {
-                XposedHelpers.findAndHookMethod(
-                    "com.android.systemui.statusbar.phone.ScrimController", it.classLoader,
-                    METHOD_NAME, GRADIENT_COLORS_CLASS, replacement)
-            } catch (e: Exception) {
-                e.printStackTrace()
+            classesToModify.forEach {
+                try {
+                    XposedHelpers.findAndHookMethod(
+                        it, param.classLoader, METHOD_NAME, GRADIENT_COLORS_CLASS, replacement)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
     }
